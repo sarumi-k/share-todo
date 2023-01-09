7df444f499606f3d2b11c32bbabe19a9a8157876
sareum <rkdtkfma@gmail.com>
2023-01-09 23:07:09 +0900

    footer 전부 삭제 구현

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>Footer</div>
+  <div class="flex justify-center">
+    <div class="w-28 py-3 bg-white hover:cursor-pointer">
+      <span class="text-red-700 block" @click="clearAll()">Clear All</span>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    clearAll() { 
+      localStorage.clear();
+    }
+  }
+};
 </script>
