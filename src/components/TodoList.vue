bcd0dc326a4bbcd63ae169323cc72a4f678ab068
sareum <rkdtkfma@gmail.com>
2023-01-12 22:33:58 +0900

    transition 적용

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex justify-center">
-    <ul class="space-y-2 w-72">
-      <li class="bg-white h-10 px-4 py-1 rounded flex justify-between items-center shadow" v-for="(list, index) in todoList" :key="index" >
-        <input type="checkbox"  @click="toggleOnOff(list, index)">
-        <class class="mr-3" :class="{ 'line-through text-gray-400': list.completed }">{{ list.item }}</class>
-        <button class="bg-red-400 text-white px-3 rounded" @click="deleteList(list.item, index)">Del</button>
-      </li>
+    <ul class="space-y-4 w-72">
+      <transition-group name="list">
+        <li class="bg-white h-10 px-4 py-1 rounded flex justify-between items-center shadow" v-for="(list, index) in todoList" :key="index" >
+          <input type="checkbox"  @click="toggleOnOff(list, index)">
+          <class class="mr-3" :class="{ 'line-through text-gray-400': list.completed }">{{ list.item }}</class>
+          <button class="bg-red-400 text-white px-3 rounded" @click="deleteList(list.item, index)">Del</button>
+        </li>
+      </transition-group>
     </ul>
   </div>
 </template>
@@ -24,3 +26,13 @@ export default {
 
 };
 </script>
+
+<style>
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to{
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
