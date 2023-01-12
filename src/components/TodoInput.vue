de00250074fd662ef987779bbcd71a15ec9d6d65
sareum <rkdtkfma@gmail.com>
2023-01-12 22:26:00 +0900

    modal 추가

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -2,22 +2,34 @@
   <div>
     <input type="text" class="rounded w-60 py-1" v-model="newTodoItem" @keyup.enter="addTodo" />
     <button @click="addTodo" class="bg-gray-400 px-3 py-1 rounded-sm ml-2">add</button>
+
+    <ModalBase v-if="showModal" @close="showModal = false">
+      <h3 slot="body">단어를 입력해주세요 :)</h3>
+    </ModalBase>
   </div>
 </template>
 
 <script>
+import ModalBase from './common/ModalBase.vue';
+
 export default {
   data: function () {
     return {
       newTodoItem: "",
+      showModal: false,
     };
+  },
+  components: {
+    ModalBase
   },
   methods: {
     addTodo: function () {
       if (this.newTodoItem !== '') {
-        this.$emit("addTodoItem", this.newTodoItem); 
+        this.$emit("addTodoItem", this.newTodoItem);
+        this.clearInput();
+      } else { 
+        this.showModal = true;
       }
-      this.clearInput();
     },
     clearInput: function () {
       this.newTodoItem = "";
