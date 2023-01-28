954e6bec9abe2f3007f01076b9cc4655a1da3cb7
sareum <rkdtkfma@gmail.com>
2023-01-28 15:38:41 +0900

    Vuex helper 적용

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,10 +2,10 @@
   <div class="flex justify-center">
     <ul class="space-y-4 w-72">
       <transition-group name="list">
-        <li class="bg-white h-10 px-4 py-1 rounded flex justify-between items-center shadow" v-for="(list, index) in this.$store.state.todoList" :key="index" >
-          <input type="checkbox"  @click="toggleOnOff(list, index)">
-          <class class="mr-3" :class="{ 'line-through text-gray-400': list.completed }">{{ list.item }}</class>
-          <button class="bg-red-400 text-white px-3 rounded" @click="deleteList(list.item, index)">Del</button>
+        <li class="bg-white h-10 px-4 py-1 rounded flex justify-between items-center shadow" v-for="(list, index) in this.storedItemsList" :key="index" >
+          <input type="checkbox"  @click="toggleOnOff({ list, index })">
+          <div class="mr-3" :class="{ 'line-through text-gray-400': list.completed }">{{ list.item }}</div>
+          <button class="bg-red-400 text-white px-3 rounded" @click="deleteList({ list, index })">Del</button>
         </li>
       </transition-group>
     </ul>
@@ -13,16 +13,18 @@
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex';
+
 export default {
   methods: {
-    deleteList(item, index) { 
-      this.$store.commit('deleteOneItem', {item, index})
-    },
-    toggleOnOff(item, index) { 
-      this.$store.commit('toggleOneItem', {item, index})
-    }
+    ...mapMutations({
+      deleteList: 'deleteOneItem',
+      toggleOnOff: 'toggleOneItem'
+    }),
   },
-
+  computed: {
+    ...mapGetters(['storedItemsList'])
+  }
 };
 </script>
 
--- src/store/store.js
+++ src/store/store.js
@@ -20,22 +20,27 @@ export const store = new Vuex.Store({
     headerText: 'My TODO List',
     todoList: storage.fetch(),
   },
+  getters: {
+    storedItemsList(state) { 
+      return state.todoList;
+    }
+  },
   mutations: {
     addOneItem(state, todoItem) { 
       const obj = { completed: false, item: todoItem };
       localStorage.setItem(todoItem, JSON.stringify(obj));
       state.todoList.push(obj);
     },
-    deleteOneItem(state, payload) { 
-      if (localStorage.getItem(payload.item)) { 
-        localStorage.removeItem(payload.item.item);
+    deleteOneItem(state, payload) {
+      if (localStorage.getItem(payload.list.item)) { 
+        localStorage.removeItem(payload.list.item);
         state.todoList.splice(payload.index, 1);
       }
     },
     toggleOneItem(state, payload) { 
       state.todoList[payload.index].completed = !state.todoList[payload.index].completed;
-      localStorage.removeItem(payload.item.item);
-      localStorage.setItem(payload.item.item, JSON.stringify(payload.item));
+      localStorage.removeItem(payload.list.item);
+      localStorage.setItem(payload.list.item, JSON.stringify(payload.item));
     },
     allItem(state) { 
       localStorage.clear();
